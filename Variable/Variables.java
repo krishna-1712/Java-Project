091db4991924182ae07c40f0fff1a442a6265f39
Krishnakumar R <krishnakumar20051712@gmail.com>
2026-09-09 15:51:27 +0530

    Add files via upload

---
+++ Variable/Variables.java
@@ -0,0 +1,39 @@
+ public class Variables {
+
+  public static void main(String[] args){
+//         String name = "Krishnakumar";
+//         int age = 20;
+//         String cname = "Rajiv Gandhi College of Engg & Tech";
+//         System.out.println(name);
+        
+//     }
+// }
+
+// int num = 45;
+// int num2 = 40;
+// System.out.println(num + num2);
+//   }
+// }
+
+// int length = 12;
+// int width = 1;
+// int area = length * width;
+// System.out.println("The length of the rectangle :" + area);
+//   }
+// }
+
+
+// int c = 5;
+// System.out.println((c*9/5)+32);
+//   }
+// }
+
+int num = 50;
+int num2 =100;
+int add = num + num2;
+int result = add - num;
+int result2 = result - num;
+System.out.println("num :" + result);
+System.out.println("num2:" + result2);
+  }
+}
